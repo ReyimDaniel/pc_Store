80f4cb131e0f1324c4daa6a53093896cbe71b3cd
danil.bobryshov <danil.bobryshov@mail.ru>
2025-03-04 17:33:38 +0300

    add pai_v1 model file

---
+++ pc_store/api_v1/models/product.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+
+
+class ProductBase(BaseModel):
+    name: str
+    price: int
+    description: str
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductUpdate(ProductBase):
+    pass
+
+
+class ProductUpdatePartial(ProductBase):
+    name: Optional[str] = None
+    price: Optional[int] = None
+    description: Optional[str] = None
+
+
+class Product(ProductBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int

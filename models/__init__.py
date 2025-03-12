622c522af00326c031e5a00bd85547ebdb520b8a
danil.bobryshov <danil.bobryshov@mail.ru>
2025-03-13 00:48:35 +0300

    refactoring class user

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,10 @@ __all__ = [
     "Base",
     "Product",
     "User",
+    "Post",
 ]
 
 from .base import Base
 from .product import Product
 from .user import User
+from .post import Post
--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
 
 class User(Base):
-    name: Mapped[str]
+    username: Mapped[str] = mapped_column(String(32), unique=True)
     email: Mapped[str]
+    description: Mapped[str]

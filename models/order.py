18c5e52659085fc4221b13084fe959bb20bc6cae
danil.bobryshov <danil.bobryshov@mail.ru>
2025-03-27 15:56:11 +0300

    create new model tables, init order, order_product_association

---
+++ models/order.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from .product import Product
+    from .order_product_association import OrderProductAssociation
+
+
+class Order(Base):
+    promocode: Mapped[Optional[str]]
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.now,
+    )
+    # products: Mapped[list["Product"]] = relationship(
+    #     secondary="order_product_association",
+    #     back_populates="orders"
+    # )
+    products: Mapped[list["OrderProductAssociation"]] = relationship(back_populates="orders")
